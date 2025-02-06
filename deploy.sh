cea92eff398352b7204be7a098569ca165a0b9c8
KESHAOYE <517034774@qq.com>
2025-02-06 14:39:00 +0800

    update: 新增版本号显示

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,28 @@ set -e
 # 生成静态文件
 yarn docs:build
 
-# 进入生成的文件夹
-cd docs/.vitepress/dist
-
 # 获取当前时间
 nowTime=$(date "+%Y%m%d%H%M%S")
 
+# 生成 `version.json` 并存入 `docs/.vitepress/`
+echo "{ \"version\": \"V${nowTime}\" }" > docs/.vitepress/version.json
+
+# 进入 `dist` 目录
+cd docs/.vitepress/dist
+
 # 初始化 Git 仓库
 git init
-git branch -m master  # 确保分支是 master
+git branch -m main  # 确保使用 main 分支
 git add -A
 git commit -m "deploy: 更新博客 - V${nowTime}"
 
 # 推送到 GitHub Pages
-git push -f https://github.com/KESHAOYE/blog.git master:gh-pages
+git push -f https://github.com/KESHAOYE/blog.git main:gh-pages
 
 # 返回上级目录
 cd -
+
+# 提交 `version.json` 到 `master` 分支
+git add docs/.vitepress/version.json
+git commit -m "更新版本号 - V${nowTime}"
+git push origin master  # 推送到 master
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,12 +2,22 @@
  * @Author: KESHAOYE
  * @Date: 2023-03-25 21:26:49
  */
-/*
- * @Author: KESHAOYE
- * @Date: 2023-03-25 21:26:49
- */
 import { defineConfig } from "vitepress";
 import { generatorSideBar } from "./utils/autoSideBar/index";
+import path from "path";
+import fs from "fs";
+
+// 读取当前版本号
+const versionFile = path.resolve(__dirname, "version.json");
+let version = "未知";
+
+if (process.env.NODE_ENV !== "production") {
+  console.log("版本号为开发环境");
+  version = "开发版本";
+} else if (fs.existsSync(versionFile)) {
+  const data = JSON.parse(fs.readFileSync(versionFile, "utf-8"));
+  version = data.version;
+}
 
 // https://vitepress.dev/reference/site-config
 export default async () =>
@@ -51,7 +61,7 @@ export default async () =>
       socialLinks: [{ icon: "github", link: "https://github.com/KESHAOYE" }],
 
       footer: {
-        message: "KESHAOYE-知识星球",
+        message: `KESHAOYE-知识星球  ${version}`,
         copyright: "Copyright © 2023-2025 KEWEI LI（2024.1.22起改版中...）",
       },
       search: {
