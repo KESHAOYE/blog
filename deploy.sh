226daad3da95158bb9f3ea2cbd90739070885a18
KESHAOYE <517034774@qq.com>
2025-02-06 14:28:23 +0800

    update: 更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,17 @@ yarn docs:build
 # 进入生成的文件夹
 cd docs/.vitepress/dist
 
-# 获取当前时间（去掉 `%` 符号）
+# 获取当前时间
 nowTime=$(date "+%Y%m%d%H%M%S")
 
-# 初始化 Git（如果没有的话）
-if [ ! -d ".git" ]; then
-  git init
-  git branch -m master  # 确保分支是 main
-fi
-
+# 初始化 Git 仓库
+git init
+git branch -m master  # 确保分支是 master
 git add -A
 git commit -m "deploy: 更新博客 - V${nowTime}"
 
-# 推送到 gh-pages 分支
+# 推送到 GitHub Pages
 git push -f https://github.com/KESHAOYE/blog.git master:gh-pages
 
-# 退出 dist 目录
+# 返回上级目录
 cd -
