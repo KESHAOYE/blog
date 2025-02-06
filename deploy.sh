8c77f98410dcd4cf8ad7a4057d601fcb7a11df1f
KESHAOYE <517034774@qq.com>
2025-02-06 14:22:49 +0800

    update: 更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# 遇到错误时终止脚本
 set -e
 
 # 生成静态文件
@@ -9,19 +9,20 @@ yarn docs:build
 # 进入生成的文件夹
 cd docs/.vitepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+# 获取当前时间（去掉 `%` 符号）
+nowTime=$(date "+%Y%m%d%H%M%S")
 
-nowTime=$(date "+%Y%m%d%H%M%S%")
+# 初始化 Git（如果没有的话）
+if [ ! -d ".git" ]; then
+  git init
+  git branch -m master  # 确保分支是 main
+fi
 
-git init
 git add -A
-git commit -m 'deploy:更新博客 - V${nowTime}'
+git commit -m "deploy: 更新博客 - V${nowTime}"
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# 推送到 gh-pages 分支
+git push -f https://github.com/KESHAOYE/blog.git master:gh-pages
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f https://github.com/KESHAOYE/blog.git master:gh-pages
-
+# 退出 dist 目录
 cd -
